dfa09446251a6e0817e4f0c6681ca2a05e04b0fc
Georgia Lewis <georgia.lewis00@gmail.com>
2020-08-29 17:51:30 +1000

    start class.py

--- class.py
+++ class.py
@@ -1,1 +1,12 @@
 import input
+
+def class_design(student_names, student_avail, class_times):
+    times = ['8  9', '9  10', '10  11', '11  12', '12  13', '13  14', '14  15', '']
+    all_classes = []
+    single_class = []
+    for instance in class_times:
+        single_class.append(instance[0])
+        single_class.append(instance[1])
+        single_class.append(instance[2])
+
+
