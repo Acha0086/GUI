17f8a193a47f8877060837587f88bbd57acb7151
Dax246 <damien.ambegoda@gmail.com>
2020-08-29 13:53:45 +1000

    no message

--- input.py
+++ input.py
@@ -2,7 +2,7 @@ import string
 
 
 # students saved as (student_name, list of lists)
-def input(Student, Units, Unit_name):
+def input(Students, Units, Unit_name):
     # Units saved as [[Class name(WS1 1), day, time], [Class name (WS2 1), day, time]]
     row_num = 0
     file = open(str(Units), "r", encoding="UTF-8-sig")
@@ -30,12 +30,27 @@ def input(Student, Units, Unit_name):
         single_class_list.append(word.translate(str.maketrans('', '', string.punctuation)).strip())
         single_class_list_counter += 1
     full_class_list.append(single_class_list)
+    file.close()
 
     # turn student input into python readable list of lists
+    student_table = []
+    for i in range(13):
+        student_table.append([])
+    file = open(str(Students), "r", encoding="UTF-8-sig")
+    line_num = -1
+    for line in file:
+        line_num += 1
+        line = line.split(',')
+        print(line)
+        for word in line:
+            if word == '':
+                student_table[line_num].append(None)
+            else:
+                student_table[line_num].append(word.strip())
+    return student_table
 
 
-
-print(input('asda', 'Class input.csv', 'FIT2014'))
+print(input('test student.csv', 'Class input.csv', 'FIT2014'))
 
 
 
