3a5456c68192185bdb95698c5f8d03b5b1cc6ef8
Dax246 <damien.ambegoda@gmail.com>
2020-08-29 13:31:37 +1000

    improved class input again

--- input.py
+++ input.py
@@ -6,31 +6,31 @@ def input(Student, Units, Unit_name):
     file = open(str(Units), "r", encoding="UTF-8-sig")
     for line in file:
         line = line.split(',')
-        print(line)
         if line[0] != str(Unit_name):
             row_num += 1
         else:
             break
-    print(line)
     skip_line = 0
     full_class_list = []
     single_class_list = []
     single_class_list_counter = 0
     for word in line:
+        if word == '':
+            break
         if skip_line == 0:
             skip_line += 1
             continue
-        while word != '':
-            if single_class_list_counter == 3:
-                full_class_list.append(single_class_list)
-                single_class_list_counter = 0
-            single_class_list.append(word)
-            single_class_list_counter += 1
+        if single_class_list_counter == 3:
+            full_class_list.append(single_class_list)
+            single_class_list_counter = 0
+            single_class_list = []
+        single_class_list.append(word)
+        single_class_list_counter += 1
     full_class_list.append(single_class_list)
+    return full_class_list
 
 
-
-input('asda', 'Class input.csv', 'FIT2014')
+print(input('asda', 'Class input.csv', 'FIT2014'))
 
 
 
