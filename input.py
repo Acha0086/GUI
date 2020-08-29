18c4a74c466ad3cc07d15dcb25b24398dd900dae
Dax246 <damien.ambegoda@gmail.com>
2020-08-29 12:01:13 +1000

    even better csv

--- input.py
+++ input.py
@@ -1,7 +1,24 @@
 
 
-def input('Student', 'Unit'):
-    # Uni
+def input(Student, Units, Unit_name):
+    # Units saved as [[Class name(WS1 1), day, time], [Class name (WS2 1), day, time]]
+    row_num = 0
+    file = open(str(Units), "r", encoding="UTF-8-sig")
+    for line in file:
+        line = line.split(',')
+        print(line)
+        if line[0] != str(Unit_name):
+            row_num += 1
+        else:
+            break
+    print(line)
+    skip_line = 
+    for word in line:
+        print(word)
+
+
+
+input('asda', 'Class input.csv', 'FIT2014')
 
 
 
